d7aeee51d8caa45bfab3a36442cd1e25a0ce837d
Hack012 <veetoveen@naver.com>
2019-07-19 16:13:04 +0900

    balace

---
+++ geteos.cpp
@@ -0,0 +1,34 @@
+#include <eosio/eosio.hpp>
+#include <eosio/asset.hpp>
+
+using namespace eosio;
+
+CONTRACT geteos: public contract {
+    public:
+      using contract::contract;
+
+      ACTION getbalance(name user, asset token) {//사용자의 잔액을 확인
+         accounts mytoken("eosio.token"_n, user.value);
+         symbol sym = token.symbol;
+         auto itr = mytoken.require_find(sym.code().raw(), "no balance");
+
+         print(itr->balance);
+      }
+    private://eosio token 테이블에 조율
+      struct [[eosio::table]] account {
+            asset    balance; //asset: Token타입
+
+            uint64_t primary_key()const { return balance.symbol.code().raw(); }
+         };
+
+         struct [[eosio::table]] currency_stats {
+            asset    supply;
+            asset    max_supply;
+            name     issuer;
+
+            uint64_t primary_key()const { return supply.symbol.code().raw(); }
+         };
+
+         typedef eosio::multi_index< "accounts"_n, account > accounts;
+         typedef eosio::multi_index< "stat"_n, currency_stats > stats;
+};
